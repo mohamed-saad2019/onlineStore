3042b4dcb2940310e372fccc7609e449f9964adc
Mohamed Saad <eng.mohameds2016@gmail.com>
2020-10-04 15:42:56 +0200

    last updated

---
+++ app/Models/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Authenticatable
+{
+    protected $table = "admins" ;
+    
+    protected $guarded = [];
+    public $timestampa = 'true';
+
+   
+    
+
+
+    
+}
